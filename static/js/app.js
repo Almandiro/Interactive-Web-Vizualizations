91cc78fe4f787db46ca3bd3af747b79f54a0fe51
Ali Daneshmand <ali.daneshmand@gmail.com>
2021-09-22 20:13:57 -0400

    modified:   static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -102,5 +102,19 @@ function selDataset(sample)
 	}
 }
 
+var json = JSON.parse(fs.readFileSync('../../sample.json').toString());
+
+console.log(json);
+//fs.readFile('./content.json', (err, data) => {
+//    if (err)
+//      console.log(err);
+//    else {
+//      var json = JSON.parse(data);
+    //your code using json object
+//    }
+//})
+
+
+
 // Initialize dashboard on page load
 init();
